0d89a9619f2743c03a5a7ebb8084a5ea9a502fbe
Aman Pratap Singh <amanprtpsingh@gmail.com>
2018-10-28 18:24:15 +0530

    Add Assembler Program

---
+++ assembler.py
@@ -0,0 +1,50 @@
+from instructions import makeInstruction
+
+address = 0
+
+print(
+    '''
+    This program assembles a 10-bit assembly program using mneunomics.
+    The assembler is non-case sensitive and White Space is ignored.
+    Registers Available :- (R0-R6), Register number should be prefixed with 'R' or 'r'.
+    Immediate value should be two digit hexadecimal and prefixed with 0x.
+    The Legal instructions available are:\n\n
+    ADD RegSrcA, RegSrcB or ADD RegSrcA, 0xF1 \n
+    SUB RegSrcA, RegSrcB or SUB RegSrcA, 0xF1 \n
+    JMP RegSrcA or JMP 0xF1 \n
+    AND RegSrcA, RegSrcB or AND RegSrcA, 0xF1 \n    
+    OR RegSrcA, RegSrcB or OR RegSrcA, 0xF1 \n    
+    NOT RegSrcA \n
+    XOR RegSrcA, RegSrcB or XOR RegSrcA, 0xF1 \n    
+    MOV RegSrcA, RegSrcB or MOV RegSrcA, 0xF1 \n
+    LOD RegSrcA, RegSrcB or MOV RegSrcA, 0xF1 \n
+    STR RegSrcA, RegSrcB or MOV RegSrcA, 0xF1 \n
+    HLT \n\n
+
+    The last instruction of Program must be `HLT`.\n
+    Enter your program one instruction per line.\n\n
+    '''
+)
+
+machineCode = 'v2.0 raw'
+instrList = []
+while True:
+    line = str(input('%s : ' % address))
+
+    try:
+        instr = makeInstruction(line)
+        instrList.append(instr)
+        address = address + 1
+        if line.strip()[:3].upper() == 'HLT':
+            break
+    except Exception as e:
+        # print('Bad Instruction: ', e)
+        raise Exception()
+    
+
+if len(instrList) > 0:
+    filename = str(input('File Name: '))
+    with open(filename, 'w') as f:
+        f.write('v2.0 raw\n')
+        for line in instrList:
+            f.write(line + '\n')
---
+++ instructions.py
@@ -0,0 +1,155 @@
+ops = { 
+    'ADD' : '0001',
+    'SUB' : '0010',
+    'JMP' : '0011',
+    'AND' : '0100',
+    'OR'  : '0101',
+    'NOT' : '0110',
+    'XOR' : '0111',
+    'MOV' : '1000',
+    'LOD' : '1001',
+    'STR' : '1010',
+    'HLT' : '1111' 
+    }
+
+registers = {
+    'R0' : '000',
+    'R1' : '001',
+    'R2' : '010',
+    'R3' : '011',
+    'R4' : '100',
+    'R5' : '101'
+}
+
+def makeInstruction(line):
+    op = line.split(',')[0].split()[0].upper()
+
+    if op not in ops:
+        raise Exception('Operation %s Not Found!' % op)
+    
+    if op == 'JMP':
+        if len(line.split(',')) > 1 or len(line.split()) != 2:
+            raise Exception('Instruction Format is Wrong')
+        op1 = line.split()[1]
+        
+        if op1.upper() in registers:
+            i = Instruction(op, False, op1.upper(), None, None, line)
+        elif op1[:2] == '0x' and len(op1) == 4:
+            num = int(op1, 16)
+            if (num > 255):
+                raise Exception('Immediate Value too big!')
+            imd = '{0:08b}'.format(num)
+            i = Instruction(op, True, None, None, imd, line)
+        else:
+            raise Exception('Bad Operand')
+        
+    elif op == 'NOT':
+        if len(line.split(',')) > 1 or len(line.split()) != 2 or line.split()[1] not in registers:
+            raise Exception('Instruction Format is Wrong')
+
+        op1 = line.split()[1]
+        i = Instruction(op, False, op1.upper(), None, None, line)
+
+
+    elif op == 'HLT':
+        if len(line.split(',')) > 1 or len(line.split()) != 1:
+            raise Exception('Instruction Format is Wrong')
+
+        i = Instruction(op, False, None, None, None, line)
+
+    else:
+        if len(line.split(',')) != 2 or len(line.split(',')[0].split()) != 2:
+            print(len(line.split(',')))
+            print(len(line.split(',')[1].split()))
+            raise Exception('Instruction Format is Wrong')
+        
+        op2 = line.split(',')[1].strip()
+
+        op1 = line.split(',')[0].split()[1].upper()
+
+        if op1 not in registers:
+            raise Exception('Bad Operand')
+        
+        if op2.upper() in registers:
+            i = Instruction(op, False, op1.upper(), op2.upper(), None, line)
+        elif op2[:2] == '0x' and len(op2) == 4:
+            num = int(str(op2), 16)
+            if (num > 255):
+                raise Exception('Immediate Value too big!')
+            imd = '{0:08b}'.format(num)
+            print(imd)
+            i = Instruction(op, True, op1.upper(), None, imd, line)
+        else:
+            raise Exception('Bad Operand')
+
+
+    return i.strInstr()
+
+
+class Instruction:
+    indirect = bool()
+    opcode = str()
+    immediate = str()
+    src = str()
+    dest = str()
+    comment = str()
+    binaryInstr = list()
+    hexInstr = list()
+
+    def __init__(self, opr, indirect, src, dest, immediate, text):
+        self.binaryInstr.clear()
+        self.hexInstr.clear()
+        if not opr in ops:
+            raise Exception('Not a valid operation!')
+
+        if not src and (opr != 'JMP' and opr != 'HLT'):
+            raise Exception('Invalid Source')
+
+        if src and not src in registers:
+            raise Exception('Invalid Source')
+
+        if not dest and not indirect and opr != 'JMP' and opr != 'NOT' and opr != 'HLT':
+            raise Exception('Invalid Destination')
+
+        if dest and not dest in registers:
+            raise Exception('Invalid Destination')
+
+        if indirect and (not immediate or len(immediate) != 8):
+            raise Exception('Invalid Immediate Value')
+
+        self.opcode = ops[opr]
+        self.indirect = indirect
+        if src:
+            self.src = registers[src]
+        if dest:
+            self.dest = registers[dest]
+        self.immediate = immediate
+        self.comment = text
+
+        if indirect:
+            self.binaryInstr.append('1')
+        else:
+            self.binaryInstr.append('0')
+
+        self.binaryInstr.extend(list(self.opcode))
+
+        if opr == 'JMP' and indirect:
+            self.binaryInstr.extend(list('000'))            
+        elif opr != 'HLT':
+            self.binaryInstr.extend(list(self.src))
+        else:
+            self.binaryInstr.extend(list('000'))
+
+        if not indirect:
+            self.binaryInstr.extend(list(self.dest))
+            self.binaryInstr.extend(list('00000'))
+        else:
+            self.binaryInstr.extend(list(self.immediate))
+
+        self.hexInstr.append( hex( int(''.join(self.binaryInstr[0:4]), 2) )[2:] )
+        self.hexInstr.append( hex( int(''.join(self.binaryInstr[4:8]), 2) )[2:] )
+        self.hexInstr.append( hex( int(''.join(self.binaryInstr[8:12]), 2) )[2:] )
+        self.hexInstr.append( hex( int(''.join(self.binaryInstr[12:16]), 2) )[2:] )
+
+    def strInstr(self):
+        return ''.join(self.hexInstr) + '   # ' + self.comment
